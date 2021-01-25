868993b93f1a3358031c2bacf43b917ce7d29214
Felix Ungman <felix@ungman.com>
2021-01-25 08:05:41 +0100

    Adjust cannon model to have one weapon per gun

--- src/units/index.ts
+++ src/units/index.ts
@@ -1,11 +1,9 @@
 import * as ash_arq from './ash-arq.json';
 import * as ash_bow from './ash-bow.json';
-import * as ash_can from './ash-can.json';
 import * as ash_kata from './ash-kata.json';
 import * as ash_nagi from './ash-nagi.json';
 import * as ash_yari from './ash-yari.json';
 import * as cav_bow from './cav-bow.json';
-import * as cav_can from './cav-can.json';
 import * as cav_kata from './cav-kata.json';
 import * as cav_nagi from './cav-nagi.json';
 import * as cav_yari from './cav-yari.json';
@@ -18,12 +16,10 @@ import * as sam_yari from './sam-yari.json';
 export {
     ash_arq,
     ash_bow,
-    ash_can,
     ash_kata,
     ash_nagi,
     ash_yari,
     cav_bow,
-    cav_can,
     cav_kata,
     cav_nagi,
     cav_yari,
