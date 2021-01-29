5fe91cb869e99d49d2a46dbc9459dd649209a03c
Felix Ungman <felix@ungman.com>
2021-01-29 08:51:10 +0100

    build new version

--- src/shapes/index.ts
+++ src/shapes/index.ts
@@ -1,3 +1,3 @@
-import particles from './particles.json';
-import vegetation from './vegetation.json';
+import * as particles from './particles.json';
+import * as vegetation from './vegetation.json';
 export { particles, vegetation }
